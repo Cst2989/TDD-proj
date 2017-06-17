fc65861e8cd408a70a3c758e2f4686a2d17b4e98
Neciu Dan <cst2989@gmail.com>
2017-06-17 15:36:24 +0300

    reidented specs

--- src/app/account.spec.ts
+++ src/app/account.spec.ts
@@ -4,8 +4,8 @@ describe('Account', () => {
   let account;
 
   beforeEach(() => {
-      //create the stack
-      account = new Account(1);
+    //create the stack
+    account = new Account(1);
   });
 
   it('should work', () => {
@@ -13,46 +13,46 @@ describe('Account', () => {
   });
 
   it('should have 0 balance', () => {
-      let balance = account.getBalance();
-      expect(balance).toEqual(0);
+    let balance = account.getBalance();
+    expect(balance).toEqual(0);
   })
 
   it('should have 100 balance after deposit', () => {
-      account.deposit(100);
-      let balance = account.getBalance();
-      expect(balance).toEqual(100);
+    account.deposit(100);
+    let balance = account.getBalance();
+    expect(balance).toEqual(100);
   });
 
   it("should add to balance if amount is a float number", () => {
-      account.deposit(100.5);
-      let balance = account.getBalance();
-      expect(balance).toEqual(100.5);
+    account.deposit(100.5);
+    let balance = account.getBalance();
+    expect(balance).toEqual(100.5);
   });
 
   it("should convert deposit of dollars to RON", () => {
-      account.deposit(100, '$');
-      let balance = account.getBalance();
-      expect(balance).toEqual(450);
+    account.deposit(100, '$');
+    let balance = account.getBalance();
+    expect(balance).toEqual(450);
   });
 
   it("should add to balance if balance is not 0", () => {
-      account = new Account(2);
-      account.deposit(100);
-      let balance = account.getBalance();
-      expect(balance).toEqual(0);
+    account = new Account(2);
+    account.deposit(100);
+    let balance = account.getBalance();
+    expect(balance).toEqual(0);
   });
 
   it("should throw exception if deposit is not in range and not modify the ballance", () => {
-      let mockData = ["-100","1005","-45.5"];
+    let mockData = ["-100", "1005", "-45.5"];
 
-      mockData.map(d=> {
-          var foo = function() {
-            account.deposit(d)
-          };
-          expect(foo).toThrowError(TypeError);
-          let balance = account.getBalance();
-          expect(balance).toEqual(0);
-      });
+    mockData.map(d => {
+      var foo = function() {
+        account.deposit(d)
+      };
+      expect(foo).toThrowError(TypeError);
+      let balance = account.getBalance();
+      expect(balance).toEqual(0);
+    });
   })
 
 });
