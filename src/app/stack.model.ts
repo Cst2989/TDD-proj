9e60163b40a8e57a36586d59140387ef4082e9eb
Neciu Dan <cst2989@gmail.com>
2017-06-17 10:48:01 +0300

    Added size variable

--- src/app/stack.model.ts
+++ src/app/stack.model.ts
@@ -1,11 +1,11 @@
 export class Stack {
     public title: String = 'app workss!';
-
+    public sizeNr: number = 0;
     constructor() {
 
     }
 
     public size(): number {
-        return 0
+        return this.sizeNr;
     }
 }
